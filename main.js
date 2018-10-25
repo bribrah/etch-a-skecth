d760aff8f7311e054148aed33bf49aabe5541a0a
bribrah <cheesemas46@gmail.com>
2018-10-24 18:35:37 -0700

    fixed loop to be more computationally efficient

--- main.js
+++ main.js
@@ -4,13 +4,14 @@ let gridBlocksPerSide = 16
 let grid = document.querySelector("#gridSystem");
 let gridBlocks;
 function generateBlockGrid(){
-    let blockArea = (grid.clientWidth /gridBlocksPerSide ) - 2;
+    let blockArea = (grid.clientWidth /gridBlocksPerSide );
     for (i = 0;  i<(gridBlocksPerSide * gridBlocksPerSide); i++){
         let gridBlock = document.createElement("div");
         gridBlock.classList.add("gridBlock");
         gridBlock.style.width = `${blockArea}px`;
         gridBlock.style.height = `${blockArea}px`;
         grid.appendChild(gridBlock);
+    }
 
             //Change block color upon mouse entering a block
         gridBlocks = document.querySelectorAll(".gridBlock");
@@ -20,7 +21,6 @@ function generateBlockGrid(){
                 });
             });
         }
-    }
 generateBlockGrid();
 //get reset button to clear the color from the grid
 let resetButton = document.querySelector("#reset");
