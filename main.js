29e92cabaa3bc51e87fc9cf0dde9d78768426942
bribrah <cheesemas46@gmail.com>
2018-10-24 18:21:38 -0700

    Added ability to use moue to etch a sketch

--- main.js
+++ main.js
@@ -1,10 +1,34 @@
+
+// Create the  grid
+let gridBlocksPerSide = 16
 let grid = document.querySelector("#gridSystem");
-let blockArea=  35/16;
-for (i = 0;  i<=16; i++){
-    for (y = 0; y<=16; y++){
+let gridBlocks;
+function generateBlockGrid(){
+    let blockArea = (grid.clientWidth /gridBlocksPerSide ) - 2;
+    for (i = 0;  i<(gridBlocksPerSide * gridBlocksPerSide); i++){
         let gridBlock = document.createElement("div");
         gridBlock.classList.add("gridBlock");
+        gridBlock.style.width = `${blockArea}px`;
+        gridBlock.style.height = `${blockArea}px`;
         grid.appendChild(gridBlock);
-        
+
+            //Change block color upon mouse entering a block
+        gridBlocks = document.querySelectorAll(".gridBlock");
+        gridBlocks.forEach((block) => {
+            block.addEventListener('mouseenter', (e) => {
+                block.style.backgroundColor = "blue";
+                });
+            });
+        }
     }
+generateBlockGrid();
+//get reset button to clear the color from the grid
+let resetButton = document.querySelector("#reset");
+function resetGrid(){
+    gridBlocks.forEach((block) => block.style.backgroundColor = "white"); 
+    gridBlocksPerSide = parseInt(prompt("How many blocks would you like per side?"));
+    grid.innerHTML = " "; 
+    generateBlockGrid();
+    
 }
+resetButton.addEventListener('click', resetGrid);
